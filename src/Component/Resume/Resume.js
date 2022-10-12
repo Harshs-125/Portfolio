88b8b3ddf2974d07d0d43ae34020be390b0c9f66
Harsh <soni.hs661@gmail.comm>
2022-10-13 00:25:20 +0530

    added second resume pdf

--- src/Component/Resume/Resume.js
+++ src/Component/Resume/Resume.js
@@ -7,7 +7,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://github.com/Harshs-125/Portfolio/blob/master/src/Assets/harsh_resume6.pdf";
 
 function Resume(props) {
   return (
